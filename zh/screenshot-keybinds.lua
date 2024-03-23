01d4e08e5f39ea5ac7452b91ca613e2f665c9fb0
x1ao4 <112841659+x1ao4@users.noreply.github.com>
2024-03-24 01:05:03 +0800

    优化截图功能
    1. 在生成文件名时去除标题中的特殊符号，避免因为标题包含特殊符号造成截图失败。
    2. 修改获取电视剧集季数与集数的方式，避免因为文件命名不规范造成截图失败。
    3. 在截图文件名中增加帧数序号，这将允许逐帧截图，避免连续截图时彼此覆盖。
    4. 添加队列功能，允许连续截图，若连续多次按下截图键，将依次生成截图请求并依次保存截图。
    5. 取消电视节目截图文件名中季与集的二位数限制，现在将会完整显示季数与集数，即使它们超过二位数。
    6. 若无法获取有效的年份信息，这个字段将会从该文件的截图文件名中移除。
    7. 优化视频类型的判断，使得无论在播放什么类型的视频时，都可以生成正确的文件名并保存截图。
    8. 在播放音频文件时截图功能将不再生效。

--- zh/screenshot-keybinds.lua
+++ zh/screenshot-keybinds.lua
@@ -1,48 +1,95 @@
 function get_media_info()
-    local media, title, release, year, media_type, season_number, episode_number
+    local show_title, title, year, s00e00
+    local media, release
     media = mp.get_property("user-data/plex/playing-media")
+
+    if not string.match(media, "\"summary\":\".-\"title\":\"(.-[%w%s%p])\"") then
+        return
+    end
+
     title=".+\"summary\":\".-\"title\":\"(.-[%w%s%p])\".+$"
     title=string.gsub(media,title,"%1")
+
     title = string.gsub(title,"[\"]","")
     title = string.gsub(title,"[,]","")
+    title = string.gsub(title,"[<>:\"/\\|?*]","")
+
     release=".+\"originallyAvailableAt\":\"(.-[%w%s%p])\".+$"
     release=string.gsub(media,release,"%1")
     year=string.sub(release, 1, 4)
-    
+
+    if year == '{"au' then
+        year = nil
+    end
+
     if string.match(media, "\"type\":\"episode\"") then
         show_title=".+\"grandparentTitle\":\"(.-[%w%s%p])\".+$"
-        	show_title=string.gsub(media,show_title,"%1")
-        s00e00=".+\"file\":\".-([Ss][0-9][0-9][Ee][0-9][0-9]).+$"
-            s00e00=string.gsub(media,s00e00,"%1")
-        
+        show_title=string.gsub(media,show_title,"%1")
+
+        local episode_index = string.match(media, "\"index\":(%d+),\"isDownloaded\"")
+        local season_index = string.match(media, "\"parentIndex\":(%d+)")
+        if episode_index and season_index then
+            s00e00 = "S" .. string.format("%02d", tonumber(season_index)) .. "E" .. string.format("%02d", tonumber(episode_index))
+        else
+            s00e00 = nil
+        end
+
         show_title = string.gsub(show_title,"[\"]","")
         show_title = string.gsub(show_title,"[,]","")
-        
-        return show_title, year, s00e00
+        show_title = string.gsub(show_title,"[<>:\"/\\|?*]","")
     else
-        return title, year
+        s00e00 = nil
     end
-    
+    return show_title, title, year, s00e00
 end
 
+local screenshot_queue = {}
+
+local function process_screenshot_queue()
+    if #screenshot_queue > 0 then
+        local screenshot_request = table.remove(screenshot_queue, 1)
+        mp.commandv("screenshot-to-file", screenshot_request)
+    end
+end
+
+mp.register_event("screenshot-filename", process_screenshot_queue)
+
 mp.add_key_binding("s", "screenshot", function()
-    local path = "/Users/User/Downloads/" -- 设置截图保存路径
+    local media = mp.get_property("user-data/plex/playing-media")
+    if string.match(media, "\"type\":\"track\"") then
+        return
+    end
+
+    local path = "/Users/User/Downloads/" -- 设置截图保存路径，例如 /Users/User/Downloads/ 或 C:\Users\User\Downloads\
     local time_pos = mp.get_property_number("time-pos")
     local seconds = math.floor(time_pos % 60)
     local minutes = math.floor((time_pos / 60) % 60)
     local hours = math.floor(time_pos / 3600)
     local time_string = string.format("%02d-%02d-%02d", hours, minutes, seconds)
-    
-    local title, year, s00e00 = get_media_info()
-    
+
+    local frame_number = mp.get_property_number("estimated-frame-number")
+    local frame_string = string.format("%04d", frame_number)
+
+    local show_title, title, year, s00e00 = get_media_info()
+
+    local screenshot_template, screenshot_filename
     if s00e00 then
-        local screenshot_template = title .. " (" .. year .. ") " .. s00e00 .. " " .. time_string .. ".png"
-        local screenshot_filename = string.format(screenshot_template)
-        mp.commandv("screenshot-to-file", path .. screenshot_filename)
+        if year then
+            screenshot_template = show_title .. " (" .. year .. ") " .. s00e00 .. " " .. time_string .. " " .. frame_string .. ".png"
+        else
+            screenshot_template = show_title .. " " .. s00e00 .. " " .. time_string .. " " .. frame_string .. ".png"
+        end
+        screenshot_filename = string.format(screenshot_template)
+        table.insert(screenshot_queue, path .. screenshot_filename)
     else
-        local screenshot_template = title .. " (" .. year .. ") " .. time_string .. ".png"
-        local screenshot_filename = string.format(screenshot_template)
-        mp.commandv("screenshot-to-file", path .. screenshot_filename)
+        if year then
+            screenshot_template = title .. " (" .. year .. ") " .. time_string .. " " .. frame_string .. ".png"
+        else
+            screenshot_template = title .. " " .. time_string .. " " .. frame_string .. ".png"
+        end
+        screenshot_filename = string.format(screenshot_template)
+        table.insert(screenshot_queue, path .. screenshot_filename)
     end
-    
+
+    process_screenshot_queue()
 end)
