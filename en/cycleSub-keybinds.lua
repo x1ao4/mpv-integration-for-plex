c06506c1a99e50e8afcca12bf87764b219855490
x1ao4 <112841659+x1ao4@users.noreply.github.com>
2024-04-06 19:24:23 +0800

    Optimize subtitle switching function
    1. Add subtitle information display feature when switching subtitles, supporting toggling between displaying Embedded Subtitle Titles or Plex Subtitle Display Titles using the “5” key.
    2. Skip subtitle formats not supported by Plex, such as “eia_608” subtitles, when switching subtitles.

--- en/cycleSub-keybinds.lua
+++ en/cycleSub-keybinds.lua
@@ -1,5 +1,79 @@
+-- Whether to show the embedded subtitle title by default
+local show_title = false
+
+function toggle_show_title()
+    show_title = not show_title
+end
+
+mp.add_key_binding("5", "toggle_show_title", toggle_show_title)
+
 function cycle_subtitles()
     mp.command("cycle sub")
+    local sub_info = mp.get_property_native("track-list")
+    local media = mp.get_property("user-data/plex/playing-media")
+    local displayTitle = "None"
+
+    for _, track in pairs(sub_info) do
+        if track["type"] == "sub" and track["selected"] then
+            if track["codec"] == "eia_608" then
+                return cycle_subtitles()
+            else
+                local ff_index = track["ff-index"]
+
+                for subtitle in string.gmatch(media, "{(.-)}") do
+                    local index = string.match(subtitle, "\"index\":(%d+)")
+                    local id = string.match(subtitle, "\"id\":\"(.-)\"")
+                    local title, display
+                    if show_title then
+                        if not track["title"] then
+                            display = string.match(subtitle, "\"displayTitle\":\"(.-)\"")
+                        else
+                            title = track["title"]
+                        end
+                    else
+                        display = string.match(subtitle, "\"displayTitle\":\"(.-)\"")
+                    end
+
+                    if ff_index ~= 0 and index and (title or display) and tonumber(index) == ff_index then
+                        displayTitle = title or display
+                        break
+                    end
+                end
+
+                if displayTitle == "None" then
+                    for subtitle in string.gmatch(media, "{(.-)}") do
+                        local id = string.match(subtitle, "\"id\":\"(.-)\"")
+                        local title, display
+                        if show_title then
+                            if string.find(track["title"], "?") then
+                                display = string.match(subtitle, "\"displayTitle\":\"(.-)\"")
+                            else
+                                title = track["title"]
+                            end
+                        else
+                            display = string.match(subtitle, "\"displayTitle\":\"(.-)\"")
+                        end
+
+                        if id and (title or display) and id == string.match(track["title"], "(%d+)") then
+                            displayTitle = title or display
+                            break
+                        end
+                    end
+                end
+            end
+
+            if displayTitle == "None" then
+                return cycle_subtitles()
+            else
+                mp.osd_message("Current Subtitle: " .. displayTitle)
+                return
+            end
+        end
+    end
+
+    if displayTitle == "None" then
+        mp.osd_message("Current Subtitle: " .. displayTitle)
+    end
 end
 
 mp.add_key_binding("t", "cycle_subtitles", cycle_subtitles)
