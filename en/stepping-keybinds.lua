89f0a983a0abf82bfd9f61e19ff8cd1da762a02a
x1ao4 <kazaf_ken@163.com>
2024-05-24 15:13:07 +0800

    优化逐帧播放功能，支持长按逐帧播放

--- en/stepping-keybinds.lua
+++ en/stepping-keybinds.lua
@@ -8,5 +8,5 @@ function stepBack()
     mp.osd_message("Frame -1", 0.5)
 end
 
-mp.add_forced_key_binding("'", "stepFwd", stepFwd)
-mp.add_forced_key_binding(";", "stepBack", stepBack)
+mp.add_forced_key_binding("'", "stepFwd", stepFwd, {repeatable=true})
+mp.add_forced_key_binding(";", "stepBack", stepBack, {repeatable=true})
